0e3e4eab01f2a5ef43326db38581f075f53d9948
Ayano <s.correa.wilches@gmail.com>
2024-03-25 13:41:30 -0500

    implement base footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,16 +1,18 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-row items-between justify-around w-full border border-black p-6">
+      <div className="flex flex-row items-between justify-around w-full border border-black p-6 text-sm">
         <div className="flex flex-col gap-4">
-          <span>I</span>
-          <span>Need</span>
-          <span>To</span>
+          <a href="#">鬼門</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Privacy</a>
         </div>
         <div className="flex flex-col gap-4">
-          <span>Do</span>
-          <span>The</span>
-          <span>Footer</span>
+          <a href="#">Want to contribute?</a>
+          <a href="#">Contact Us</a>
+          <a href="https://www.ayano.software/" target="_blank">
+            Made by Ayano
+          </a>
         </div>
       </div>
     </footer>
--- app/components/HeroOption.tsx
+++ app/components/HeroOption.tsx
@@ -6,7 +6,6 @@ export default function HeroOption({ value, ...props }: ISelect) {
   return (
     <button
       {...props}
-      // className="flex items-center justify-center border p-4 border-gray-400 hover:border-gray-600 rounded"
       className="group flex h-min items-center justify-center p-1 text-center font-medium focus:z-10 bg-white border border-gray-200 enabled:hover:bg-gray-100 enabled:hover:text-cyan-700 focus:text-cyan-700 rounded-lg focus:ring-2"
     >
       <span className="flex items-center rounded-md text-xl px-4 py-2">
