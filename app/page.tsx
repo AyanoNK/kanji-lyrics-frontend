a43143bdb8385e2223abf2aa11d4d5e65a01362c
Ayano <s.correa.wilches@gmail.com>
2024-03-30 02:42:33 -0500

    implement video player in hero view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-import HeroOption from './components/HeroOption';
+import HeroSongTest from './HeroSongTest';
+import crossingField from './songs/crossingField';
 
 export default function Home() {
-  const options: string[] = ['多角', '高く', '他覚', '多覚'];
-
   return (
     <main className="flex flex-col items-center justify-between w-full gap-16 px-8 md:px-0">
       <div className="flex flex-col items-center gap-8">
@@ -12,29 +10,7 @@ export default function Home() {
           Practice Kanji through Japanese music!
         </h2>
       </div>
-      <div className="grid grid-cols-5 gap-4 w-full">
-        <div className="col-span-5 text-left w-full">
-          <h3 className="text-2xl">Try to select the correct kanji</h3>
-          <h4 className="text-lg">
-            Listen to the song and select which kanji is being used.
-          </h4>
-        </div>
-        <div className="col-span-5 md:col-span-3 flex flex-col gap-4">
-          <iframe
-            className="w-full min-h-72"
-            src="https://www.youtube.com/embed/KId6eunoiWk?start=45&end=50&frameborder=0&controls=0&loop=1"
-            title="LiSA - Crossing Field"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          />
-          <span className="text-xl text-center">夢で ____ 飛んだ</span>
-        </div>
-
-        <div className="col-span-5 md:col-span-2 flex flex-col gap-4">
-          {options.map((option: string, index: number) => (
-            <HeroOption key={`option-${index}`} value={option} />
-          ))}
-        </div>
-      </div>
+      <HeroSongTest song={crossingField} />
     </main>
   );
 }
